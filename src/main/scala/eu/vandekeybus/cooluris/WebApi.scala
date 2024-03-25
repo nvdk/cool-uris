81e4c6708f00056eaaedee49ac0f82648ef1cca4
Niels Vandekeybus <progster@gmail.com>
2024-03-25 12:10:55 +0100

    support both schemes in URI lookup
    since http and https is a bit of a mess, change the behaviour to look up uris in
    both schemes

--- src/main/scala/eu/vandekeybus/cooluris/WebApi.scala
+++ src/main/scala/eu/vandekeybus/cooluris/WebApi.scala
@@ -3,6 +3,8 @@ package eu.vandekeybus.cooluris
 import org.scalatra._
 import org.eclipse.rdf4j.repository._
 import org.eclipse.rdf4j.query._
+import scala.util.Try
+import java.net.URLEncoder
 
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -21,7 +23,13 @@ class WebApi(repository: Repository) extends ScalatraServlet {
     if (uri.isEmpty) {
       halt(400, """{ "error": "query parameter uri is required"}""")
     }
-    val result = getRepresentations(uri) getOrElse halt(404, s"""{ "error": "no linked data representation of $uri was found"}""")
+
+    val uris = convertUrl(uri)
+    if (uris.isEmpty) {
+      halt(400, """{ "error": "query parameter uri is invalid"}""")
+    }
+
+    val result = getRepresentations(uris) getOrElse halt(404, s"""{ "error": "no linked data representation of $uri was found"}""")
 
     if (acceptsRDF && stringVal(result, "dataURL").nonEmpty)
       halt(303,"", Map("Location" -> stringVal(result, "dataURL")))
@@ -40,15 +48,21 @@ class WebApi(repository: Repository) extends ScalatraServlet {
     else
       ""
   }
-  private def getRepresentations(uri: String): Option[BindingSet] = {
+
+  private def getRepresentations(uris: Set[String]): Option[BindingSet] = {
     val con = repository.getConnection
+    val valuesClause = uris.map(url => s"<$url>").mkString(" ", "\n", " ")
     val queryString = s"""
                   SELECT ?pageURL ?dataURL
                   WHERE {
-                    OPTIONAL { <$uri> <http://www.w3.org/2000/01/rdf-schema#isDefinedBy> ?dataURL. }
-                    OPTIONAL { <$uri> <http://xmlns.com/foaf/0.1/page> ?pageURL. }
+                    VALUES ?uri {
+                       $valuesClause
+                    }
+                    { ?uri <http://www.w3.org/2000/01/rdf-schema#isDefinedBy> ?dataURL. }
+                    UNION
+                    { ?uri <http://xmlns.com/foaf/0.1/page> ?pageURL. }
                   } LIMIT 1
-                """
+                """.stripMargin
     val result = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString).evaluate
     if (result.hasNext) {
       val r = result.next
@@ -59,6 +73,21 @@ class WebApi(repository: Repository) extends ScalatraServlet {
     }
     else
       None
+  }
+
+  def convertUrl(urlParam: String): Set[String] = {
+    Try(new java.net.URL(urlParam)).toOption match {
+      case Some(url) =>
+        val path = new java.net.URI(null, null, url.getPath, null).toASCIIString()
+        val httpUrl = s"http://${url.getHost}${path}"
+        val httpsUrl = s"https://${url.getHost}${path}"
+
+        // Return the URLs as a set
+        Set(httpUrl, httpsUrl)
+      case None =>
+        // Handle invalid URL case
+        Set.empty[String]
+    }
   }
 
   private def acceptsRDF:Boolean = {
